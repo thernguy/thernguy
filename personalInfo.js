f939c039105263d74702f37b1b16563fcf6c3835
Mamun Mahmood <mamamun1999@gmail.com>
2023-11-10 08:05:27 +0600

    Create personalInfo.js

---
+++ personalInfo.js
@@ -0,0 +1,162 @@
+const socialLinks = [
+  {
+    name: "GitHub",
+    url: "http://github.com/mamun-mahmood",
+    image: "/icons/github.svg",
+  },
+  {
+    name: "Twitter",
+    url: "http://twitter.com/mamunmahmood",
+    image: "/icons/twitter.svg",
+  },
+  {
+    name: "LinkedIn",
+    url: "http://linkedin.com/in/mamunmahmood",
+    image: "/icons/linkedin.svg",
+  },
+  {
+    name: "Email",
+    url: "mailto",
+    image: "/icons/email.svg",
+  },
+];
+const contacts = [
+  {
+    value: "8801799464391",
+    name: "Phone",
+    image: "/icons/phone.svg",
+  },
+  {
+    value: "mamamun1999@gamil.com",
+    name: "Email",
+    image: "/icons/email-orange.svg",
+  },
+  {
+    value: "Dhaka, Bangladesh",
+    name: "Location",
+    image: "/icons/location.svg",
+  },
+];
+ const whatIDo = [
+    {
+      name: "Frontend Development",
+      description: "I can build a beautiful and scalable SPA using React.js",
+      image: "/icons/frontend.svg",
+      techs: [
+        {
+          name: "React.js",
+          image: "/icons/react.svg",
+          url: ""
+        },
+        {
+          name: "Next.js",
+          image: "/icons/nextjs.svg",
+          url: ""
+        },
+
+        {
+          name: "JavaScript",
+          image: "/icons/javascript.svg",
+          url: ""
+        },
+        {
+          name: "TypeScript",
+          image: "/icons/typescript.svg",
+          url: ""
+        },
+        {
+          name: "Redux",
+          image: "/icons/redux.svg",
+          url: ""
+        },
+        {
+          name: "Tailwind CSS",
+          image: "/icons/tailwind.svg",
+          url: ""
+        },
+        {
+          name: "Material UI",
+          image: "/icons/material.svg",
+          url: ""
+        },
+        {
+          name: "Bootstrap",
+          image: "/icons/bootstrap.svg",
+          url: ""
+        },
+
+      ]
+    },
+    {
+      name: "Backend Development",
+      description:
+        "I can handle database, server, api using Node.js, Express.js, MongoDB, and Firebase",
+      image: "/icons/backend.svg",
+      techs: [
+        {
+          name: "Node.js",
+          image: "/icons/nodejs.svg",
+          url: "",
+        },
+        {
+          name: "Express.js",
+          image: "/icons/expressjs.svg",
+          url: "",
+        },
+        {
+          name: "MongoDB",
+          image: "/icons/mongodb.svg",
+          url: "",
+        },
+        {
+          name: "Firebase",
+          image: "/icons/firebase.svg",
+          url: "",
+        },
+        {
+          name: "MySQL",
+          image: "/icons/mysql.svg",
+          url: "",
+        },
+        {
+          name: "RESTapi",
+          image: "/icons/api.svg",
+          url: "",
+        },
+      ]
+    },
+    {
+      name: "Mobile App Development",
+      description: "I can build android and ios apps using React Native",
+      image: "/icons/app.svg",
+      techs: [
+        {
+          name: "React Native",
+          image: "/icons/react-native.svg",
+          url: "https://reactnative.dev/",
+        },
+        {
+          name: "Expo",
+          image: "/icons/expo.svg",
+          url: "https://expo.dev/@mamunmahmood",
+        },
+      ]
+    },
+    {
+      name: "Write Technical Blogs",
+      description: "I love to write technical blogs on my website and dev.to",
+      image: "/icons/write.svg",
+      techs: [
+        {
+          name: "Dev.to",
+          image: "/icons/devto.svg",
+          url: "https://dev.to/mamunmahmood",
+        },
+        {
+          name: "Medium",
+          image: "/icons/medium.svg",
+          url: "https://mamun1999.medium.com/",
+        },
+      ]
+    },
+  ];

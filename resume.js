748dd01f186063d12dd768ed4fb220a75004ae5d
Mamun Mahmood <mamamun1999@gmail.com>
2023-11-20 12:16:28 +0600

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,55 +1,81 @@
-const contacts = {
-  name: "Mamun Mahmood",
-    email: "mamamun1999@gmail.com",
-    phone: "8801799464391",
-    location: "Dhaka, Bangladesh",
-    linkedin: "https://www.linkedin.com/in/mamunmahmood/",
-    github: "https://github.com/mamun-mahmood/",
-    twitter: "https://twitter.com/mamunmah",
- }
-const objective = "Results-driven Full Stack developer with 2 years of hands-on experience in front-end and back-end
-development. Seeking a dynamic role that allows me to leverage my expertise in JavaScript, React.js,
-Nodejs related stach to contribute in exciting product development and learn further."
-const skills = {
-    tech: ["JavaScript", "Typescript", "React", "Redux", "REST API", "Nodejs", "Express", "MongoDB", "MySQL", "React Native", "Expo"],
-    soft: ["Communication", "Clean Code", "Adaptability", "Problem-solving", "Teamwork", "Discipline"],
-    tools: ["Git", "Github", "VS Code", "Chrome Dev Tool", "Postman", "Figma", "Trello", "Firebase", "DigitalOcean", "Cloudways"]
-  }
-const experience = [
+{
+  "contacts": {
+    "name": "Mamun Mahmood",
+    "email": "mamamun1999@gmail.com",
+    "phone": "8801799464391",
+    "location": "Dhaka, Bangladesh",
+    "linkedin": "https://www.linkedin.com/in/mamunmahmood/",
+    "github": "https://github.com/mamun-mahmood/",
+    "twitter": "https://twitter.com/mamunmah"
+  },
+  "objective": "Results-driven Full Stack developer with 2 years of hands-on experience...",
+  "skills": {
+    "tech": [
+      "JavaScript",
+      "Typescript",
+      "React",
+      "Redux",
+      "REST API",
+      "Nodejs",
+      "Express",
+      "MongoDB",
+      "MySQL",
+      "React Native",
+      "Expo"
+    ],
+    "soft": [
+      "Communication",
+      "Clean Code",
+      "Adaptability",
+      "Problem-solving",
+      "Teamwork",
+      "Discipline"
+    ],
+    "tools": [
+      "Git",
+      "Github",
+      "VS Code",
+      "Chrome Dev Tool",
+      "Postman",
+      "Figma",
+      "Trello",
+      "Firebase",
+      "DigitalOcean",
+      "Cloudways"
+    ]
+  },
+  "experience": [
     {
-      title: "Software Developer (Remote)",
-      company: "Megahoot Technologies Inc",
-      company_url: "https://megahoot.com/",
-      logo_path: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKDTWDTTIx7ayvF78wTFdQeDIh-7hgM07b4QRALA5_nU-nOeCofJkxSrga3_ee1ckxSTY",
-      duration: "Februray 2022 - March 2023",
-      location: "Florida, USA",
-      description: [
+      "title": "Software Developer (Remote)",
+      "company": "Megahoot Technologies Inc",
+      "company_url": "https://megahoot.com/",
+      "logo_path": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKDTWDTTIx7ayvF78wTFdQeDIh-7hgM07b4QRALA5_nU-nOeCofJkxSrga3_ee1ckxSTY",
+      "duration": "Februray 2022 - March 2023",
+      "location": "Florida, USA",
+      "description": [
         "Led the implementation of user verification badges, link previews, and designation systems in Soapbox, using JavaScript and React.js. Git",
         "Built the whole front, and created REST API with Node.js for HootDex. Git",
-        "Developed a backend app for ChatHive (chat app) with NodeJs, SocketIO, mySQL.Git.",
+        "Developed a backend app for ChatHive (chat app) with NodeJs, SocketIO, mySQL.Git."
       ],
-      color: "#0879bf",
+      "color": "#0879bf"
     },
     {
-      title: "Freelance Developer",
-      company: "Marketplace",
-      company_url: "",
-      logo_path: "",
-      duration: "April 2023 - ",
-      location: "",
-      description: [
-        "After last job, I worked as a freelance developer. Delivered 4 web application projects in 6 months, earning a 5/5 client satisfaction.",
+      "title": "Freelance Developer",
+      "company": "Marketplace",
+      "duration": "April 2023 - ",
+      "description": [
+        "After last job, I worked as a freelance developer. Delivered 4 web application projects in 6 months, earning a 5/5 client satisfaction."
       ],
-      color: "#0879bf",
-    },
-  ]
-const projects = [
+      "color": "#0879bf"
+    }
+  ],
+  "projects": [
     {
-      name: "CompleteGreet",
-      description: "CompleteGreet is a SASS platform that combines live text chat and video bubbles in perfect harmony, It let's you to create chat bubble and embed it in your website with features like live chats, calls.",
-      url: "https://completegreet.com",
-      image: "https://completegreet.com/_next/image?url=%2Fimages%2Ftop-logo.png&w=256&q=75",
-      techs: [
+      "name": "CompleteGreet",
+      "description": "CompleteGreet is a SASS platform that combines live text chat and video bubbles in perfect harmony, It let's you to create chat bubble and embed it in your website with features like live chats, calls.",
+      "url": "https://completegreet.com",
+      "image": "https://completegreet.com/_next/image?url=%2Fimages%2Ftop-logo.png&w=256&q=75",
+      "techs": [
         "Next.js",
         "Tailwind CSS",
         "Node.js",
@@ -58,28 +84,36 @@ const projects = [
         "Socket.io",
         "Cloudways",
         "Cloudinary",
-        "Stripe",
+        "Stripe"
       ],
-      works: [
+      "works": [
         "Created SAAS a platform for embeddable video bubbles with live chat features.",
-        "Optimized video loading with CDN, implemented chat bubble API, and added features like scheduling", "open hours for live chat and call.", "Tracked analytics (views, clicks, watch time)", "optimized frontend using Next.js (SSR-CSR)",
-        "Migrated production server from DigitalOcean to Render.",
+        "Optimized video loading with CDN, implemented chat bubble API, and added features like scheduling",
+        "open hours for live chat and call.",
+        "Tracked analytics (views, clicks, watch time)",
+        "optimized frontend using Next.js (SSR-CSR)",
+        "Migrated production server from DigitalOcean to Render."
       ]
     }
-  ]
-const leadership = "At my previous job, took the lead in building the backend for a React-Native chat application. Managed two intern backend developers, initiating regular team meetings to ensure alignment and progress tracking.Successfully delivered the project on time."
-const extraCurricular = [
+  ],
+  "leadership": "At my previous job, took the lead in building the backend for a React-Native chat application. Managed two intern backend developers, initiating regular team meetings to ensure alignment and progress tracking.Successfully delivered the project on time.",
+  "extraCurricular": [
     "Share insights on React, Node.js, and JavaScript tips and tricks on LinkedIn.",
     "Avid guitar player and music enthusiast.",
     "Passionate about fitness."
-  ]
-const education = {
-    school: "University of Global Village, Barisal, Bangladesh.",
-    degree: "B.Sc. in Computer Science and Engineering (Dropout))",
-    duration: "2019 - 2020",
-    cgpa: "3.00/4.00",
-    creditedScore: "70",
-    courseWorks: [
-      "DBMS", "DSA", "OOP", "C Programming", "Algorithm Design"
+  ],
+  "education": {
+    "school": "University of Global Village, Barisal, Bangladesh.",
+    "degree": "B.Sc. in Computer Science and Engineering (Dropout))",
+    "duration": "2019 - 2020",
+    "cgpa": "3.00/4.00",
+    "creditedScore": "70",
+    "courseWorks": [
+      "DBMS",
+      "DSA",
+      "OOP",
+      "C Programming",
+      "Algorithm Design"
     ]
-  };
+  }
+}

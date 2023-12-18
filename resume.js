7db27dd6ebf51a2dcf7c7056622043732e34786c
Mamun Mahmood <mamamun1999@gmail.com>
2023-12-19 00:28:30 +0600

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,175 +1,183 @@
 {
-  "contacts": {
-    "name": "Mamun Mahmood",
-    "email": "mamamun1999@gmail.com",
-    "phone": "8801799464391",
-    "location": "Dhaka, Bangladesh",
-    "linkedin": "https://www.linkedin.com/in/mamunmahmood/",
-    "github": "https://github.com/mamun-mahmood/",
-    "twitter": "https://twitter.com/mamunmah"
-  },
-  "objective": "Results-driven Full Stack developer with 2 years of hands-on experience in front-end and back-end development. Seeking a dynamic role that allows me to leverage my expertise in JavaScript, React.js, Nodejs related stach to contribute in exciting product development and learn further.",
-  "skills": {
-    "tech": [
-      "JavaScript",
-      "Typescript",
-      "React",
-      "Redux",
-      "REST API",
-      "Nodejs",
-      "Express",
-      "MongoDB",
-      "MySQL",
-      "React Native",
-      "Expo"
-    ],
-    "soft": [
-      "Communication",
-      "Clean Code",
-      "Adaptability",
-      "Problem-solving",
-      "Teamwork",
-      "Discipline"
-    ],
-    "tools": [
-      "Git",
-      "Github",
-      "VS Code",
-      "Chrome Dev Tool",
-      "Postman",
-      "Figma",
-      "Trello",
-      "Firebase",
-      "DigitalOcean",
-      "Cloudways"
-    ]
-  },
-  "experience": [
-    {
-      "title": "Software Developer (Remote)",
-      "company": "Megahoot Technologies Inc",
-      "company_url": "https://megahoot.com/",
-      "logo_path": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKDTWDTTIx7ayvF78wTFdQeDIh-7hgM07b4QRALA5_nU-nOeCofJkxSrga3_ee1ckxSTY",
-      "duration": "Februray 2022 - March 2023",
-      "location": "Florida, USA",
-      "description": [
-        "Led the implementation of user verification badges, link previews, and designation systems in Soapbox, using JavaScript and React.js. Git",
-        "Built the whole front, and created REST API with Node.js for HootDex. Git",
-        "Developed a backend app for ChatHive (chat app) with NodeJs, SocketIO, mySQL.Git."
-      ],
-      "color": "#0879bf"
-    },
-    {
-      "title": "Freelance Developer",
-      "company": "Marketplace",
-      "duration": "April 2023 - ",
-      "description": [
-        "After last job, I worked as a freelance developer. Delivered 4 web application projects in 6 months, earning a 5/5 client satisfaction."
-      ],
-      "color": "#0879bf"
-    }
-  ],
-  "projects": [
-    {
-      "name": "CompleteGreet",
-      "description": "CompleteGreet is a SASS platform that combines live text chat and video bubbles in perfect harmony, It let's you to create chat bubble and embed it in your website with features like live chats, calls.",
-      "url": "https://completegreet.com",
-      "image": "https://completegreet.com/_next/image?url=%2Fimages%2Ftop-logo.png&w=256&q=75",
-      "techs": [
-        "Next.js",
-        "Tailwind CSS",
-        "Node.js",
-        "Express.js",
-        "SQL",
-        "Socket.io",
-        "Cloudways",
-        "Cloudinary",
-        "Stripe"
-      ],
-      "works": [
-        "Created SAAS a platform for embeddable video bubbles with live chat features.",
-        "Optimized video loading with CDN, implemented chat bubble API, and added features like scheduling",
-        "open hours for live chat and call.",
-        "Tracked analytics (views, clicks, watch time)",
-        "optimized frontend using Next.js (SSR-CSR)",
-        "Migrated production server from DigitalOcean to Render."
-      ]
-    }
-  ],
-  "leadership": "At my previous job, I took the lead in building the backend for a React-Native chat application. Managed two intern backend developers, initiating regular team meetings to ensure alignment and progress tracking.Successfully delivered the project on time.",
-  "extraCurricular": [
-    "Share insights on React, Node.js, and JavaScript tips and tricks on LinkedIn.",
-    "Avid guitar player and music enthusiast.",
-    "Passionate about fitness."
-  ],
-  "education": {
-    "school": "University of Global Village, Barisal, Bangladesh.",
-    "degree": "B.Sc. in Computer Science and Engineering (Dropout))",
-    "duration": "2019 - 2020",
-    "cgpa": "3.00/4.00",
-    "creditedScore": "70",
-    "courseWorks": ["DBMS", "DSA", "OOP", "C Programming", "Algorithm Design"]
-  },
-  "works": [
-    {
-      "title": "Complete Greet",
-      "description": "A website that lets you to create a chat bubble and embed it in your website with features like live chats, and calls.",
-      "url": "https://completegreet.com",
-      "images": [
-        "https://i.ibb.co/Wk10Fsj/completegreet1.png",
-        "https://i.ibb.co/VTv3MgZ/completegreet2.png",
-        "https://i.ibb.co/ZMn2qgz/completegreet3.png"
-      ],
-      "techs": [
-        "React",
-        "Next.js",
-        "Redux",
-        "Tailwind CSS",
-        "Node.js",
-        "Express.js",
-        "SQLite",
-        "Stripe API"
-      ]
-    },
-    {
-      "title": "Hoodex",
-      "description": "A Decentralized Digital Asset Swapping System",
-      "url": "https://www.hootdex.net/",
-      "images": [
-        "https://raw.githubusercontent.com/mamun-mahmood/hootdexFrontend/master/Screenshot%20from%202023-12-03%2023-58-34.png"
-      ],
-      "techs": ["React", "ApexCharts", "MUI", "Nodejs", "Solidity", "Crypto Wallets"]
-    },
-    {
-      "title": "MyFlix",
-      "description": "Find the latest movies and TV shows in no time",
-      "url": "https://myflix-lake-six.vercel.app/",
-      "images": [
-        "https://i.ibb.co/VBcjXFG/myflix1.png",
-        "https://i.ibb.co/JyhxyQq/myflix2.png",
-        "https://i.ibb.co/gj7xbLM/myflix3.png"
-      ],
-      "techs": ["React", "Redux", "Styled Components", "The Movie DB API"]
-    },
-    {
-      "title": "ChatterFly",
-      "description": "A RealTime Chat Application. Built with React, MUI, and Firebase.",
-      "url": "https://chatterfly.vercel.app/",
-      "images": [
-        "https://raw.githubusercontent.com/mamun-mahmood/chatterfly/main/docs/Screenshot%20from%202023-08-08%2009-45-25.png",
-        "https://raw.githubusercontent.com/mamun-mahmood/chatterfly/main/docs/Screenshot%20from%202023-08-08%2009-45-01.png"
-      ],
-      "techs": ["React", "Redux", "MUI", "Firebase"]
-    },
-    
-    {
-      "title": "BlueTrade",
-      "description": "A Dashboard Layout built with React, ApexCharts, and Vanilla CSS.",
-      "url": "https://bluetrade.vercel.app/",
-      "images": [
-        "https://raw.githubusercontent.com/mamun-mahmood/react-dashboard/main/docs/images/Screenshot%20from%202023-08-31%2020-52-00.png"
-      ],
-      "techs": ["React", "ApexCharts", "Vanilla CSS"]
-    }
-  ]
+"contacts": {
+"name": "Mamun Mahmood",
+"email": "mamamun1999@gmail.com",
+"phone": "8801799464391",
+"location": "Dhaka, Bangladesh",
+"linkedin": "https://www.linkedin.com/in/mamunmahmood/",
+"github": "https://github.com/mamun-mahmood/",
+"twitter": "https://twitter.com/mamunmah"
+},
+"objective": "Results-driven Full Stack developer with 2 years of hands-on experience in front-end and back-end development. Seeking a dynamic role that allows me to leverage my expertise in JavaScript, React.js, Nodejs related stach to contribute in exciting product development and learn further.",
+"skills": {
+"tech": [
+"JavaScript",
+"Typescript",
+"React",
+"Redux",
+"REST API",
+"Nodejs",
+"Express",
+"MongoDB",
+"MySQL",
+"React Native",
+"Expo"
+],
+"soft": [
+"Communication",
+"Clean Code",
+"Adaptability",
+"Problem-solving",
+"Teamwork",
+"Discipline"
+],
+"tools": [
+"Git",
+"Github",
+"VS Code",
+"Chrome Dev Tool",
+"Postman",
+"Figma",
+"Trello",
+"Firebase",
+"DigitalOcean",
+"Cloudways"
+]
+},
+"experience": [
+{
+"title": "Software Developer (Remote)",
+"company": "Megahoot Technologies Inc",
+"company_url": "https://megahoot.com/",
+"logo_path": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKDTWDTTIx7ayvF78wTFdQeDIh-7hgM07b4QRALA5_nU-nOeCofJkxSrga3_ee1ckxSTY",
+"duration": "Februray 2022 - March 2023",
+"location": "Florida, USA",
+"description": [
+"Led the implementation of user verification badges, link previews, and designation systems in Soapbox, using JavaScript and React.js. Git",
+"Built the whole front, and created REST API with Node.js for HootDex. Git",
+"Developed a backend app for ChatHive (chat app) with NodeJs, SocketIO, mySQL.Git."
+],
+"color": "#0879bf"
+},
+{
+"title": "Freelance Developer",
+"company": "Marketplace",
+"duration": "April 2023 - ",
+"description": [
+"After last job, I worked as a freelance developer. Delivered 4 web application projects in 6 months, earning a 5/5 client satisfaction."
+],
+"color": "#0879bf"
+}
+],
+"projects": [
+{
+"name": "CompleteGreet",
+"description": "CompleteGreet is a SASS platform that combines live text chat and video bubbles in perfect harmony, It let's you to create chat bubble and embed it in your website with features like live chats, calls.",
+"url": "https://completegreet.com",
+"image": "https://completegreet.com/_next/image?url=%2Fimages%2Ftop-logo.png&w=256&q=75",
+"techs": [
+"Next.js",
+"Tailwind CSS",
+"Node.js",
+"Express.js",
+"SQL",
+"Socket.io",
+"Cloudways",
+"Cloudinary",
+"Stripe"
+],
+"works": [
+"Created SAAS a platform for embeddable video bubbles with live chat features.",
+"Optimized video loading with CDN, implemented chat bubble API, and added features like scheduling",
+"open hours for live chat and call.",
+"Tracked analytics (views, clicks, watch time)",
+"optimized frontend using Next.js (SSR-CSR)",
+"Migrated production server from DigitalOcean to Render."
+]
+}
+],
+"leadership": "At my previous job, I took the lead in building the backend for a React-Native chat application. Managed two intern backend developers, initiating regular team meetings to ensure alignment and progress tracking.Successfully delivered the project on time.",
+"extraCurricular": [
+"Share insights on React, Node.js, and JavaScript tips and tricks on LinkedIn.",
+"Avid guitar player and music enthusiast.",
+"Passionate about fitness."
+],
+"education": {
+"school": "University of Global Village, Barisal, Bangladesh.",
+"degree": "B.Sc. in Computer Science and Engineering (Dropout))",
+"duration": "2019 - 2020",
+"cgpa": "3.00/4.00",
+"creditedScore": "70",
+"courseWorks": ["DBMS", "DSA", "OOP", "C Programming", "Algorithm Design"]
+},
+"works": [
+{
+"title": "Complete Greet",
+"description": "A website that lets you to create a chat bubble and embed it in your website with features like live chats, and calls.",
+"url": "https://completegreet.com",
+"images": [
+"https://i.ibb.co/Wk10Fsj/completegreet1.png",
+"https://i.ibb.co/VTv3MgZ/completegreet2.png",
+"https://i.ibb.co/ZMn2qgz/completegreet3.png"
+],
+"techs": [
+"React",
+"Next.js",
+"Redux",
+"Tailwind CSS",
+"Node.js",
+"Express.js",
+"SQLite",
+"Stripe API"
+]
+},
+{
+"title": "Hoodex",
+"description": "A Decentralized Digital Asset Swapping System",
+"url": "https://www.hootdex.net/",
+"images": [
+"https://raw.githubusercontent.com/mamun-mahmood/hootdexFrontend/master/Screenshot%20from%202023-12-03%2023-58-34.png"
+],
+"techs": ["React", "ApexCharts", "MUI", "Nodejs", "Solidity", "Crypto Wallets"]
+},
+{
+"title": "MyFlix",
+"description": "Find the latest movies and TV shows in no time",
+"url": "https://myflix-lake-six.vercel.app/",
+"images": [
+"https://i.ibb.co/VBcjXFG/myflix1.png",
+"https://i.ibb.co/JyhxyQq/myflix2.png",
+"https://i.ibb.co/gj7xbLM/myflix3.png"
+],
+"techs": ["React", "Redux", "Styled Components", "The Movie DB API"]
+},
+{
+"title": "ChatterFly",
+"description": "A RealTime Chat Application. Built with React, MUI, and Firebase.",
+"url": "https://chatterfly.vercel.app/",
+"images": [
+"https://raw.githubusercontent.com/mamun-mahmood/chatterfly/main/docs/Screenshot%20from%202023-08-08%2009-45-25.png",
+"https://raw.githubusercontent.com/mamun-mahmood/chatterfly/main/docs/Screenshot%20from%202023-08-08%2009-45-01.png"
+],
+"techs": ["React", "Redux", "MUI", "Firebase"]
+},
+{
+"title": "WhiteBoard",
+"description": "Whiteboard app with basic features. Creating shapes, drawing, and adding text.",
+"url": "https://whiteboardx.vercel.app/",
+"images": [
+"https://raw.githubusercontent.com/mamun-mahmood/whiteboard/master/docs/ss/p1.png"
+],
+"techs": ["React", "TailWind CSS", "React Konva", Nodejs, "Express", "MongoDB" ]
+},
+{
+"title": "BlueTrade",
+"description": "A Dashboard Layout built with React, ApexCharts, and Vanilla CSS.",
+"url": "https://bluetrade.vercel.app/",
+"images": [
+"https://raw.githubusercontent.com/mamun-mahmood/react-dashboard/main/docs/images/Screenshot%20from%202023-08-31%2020-52-00.png"
+],
+"techs": ["React", "ApexCharts", "Vanilla CSS"]
+}
+]
 }

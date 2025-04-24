b4d9991e6f7068b485ba9ea5833f1bc954f7c1e7
M A Mamun <mamamun1999@gmail.com>
2025-04-24 23:28:10 +0600

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,254 +1,229 @@
 {
-  "contacts": {
-    "name": "M A Mamun",
-    "email": "mamamun1999@gmail.com",
-    "phone": "8801799464391",
-    "location": "Dhaka, Bangladesh",
-    "linkedin": "https://www.linkedin.com/in/mamunmahmood/",
-    "github": "https://github.com/thernguy",
-    "twitter": "https://twitter.com/mamun_undefined"
-  },
-  "objective": "I build reliable, performant mobile apps using React Native and modern full-stack tools like Node.js, Next.js, Firebase, and Supabase. With 3+ years of hands-on experience, I’ve worked on e-commerce, loyalty, and SaaS platforms—handling everything from clean UI implementation to backend API development and deployment. Focused on writing maintainable code and solving real user problems.",
-  "skills": {
-    "languages": [
-      "JavaScript",
-      "Typescript"
+    "contacts": {
+        "name": "M A Mamun",
+        "email": "mamamun1999@gmail.com",
+        "phone": "8801799464391",
+        "location": "Dhaka, Bangladesh",
+        "linkedin": "https://www.linkedin.com/in/mamunmahmood/",
+        "github": "https://github.com/thernguy",
+        "twitter": "https://twitter.com/mamun_undefined"
+    },
+    "objective": "I build reliable, performant mobile apps using React Native and modern full-stack tools like Node.js, Next.js, Firebase, and Supabase. With 3+ years of hands-on experience, I’ve worked on e-commerce, loyalty, and SaaS platforms—handling everything from clean UI implementation to backend API development and deployment. Focused on writing maintainable code and solving real user problems.",
+    "skills": {
+        "languages": [
+            "JavaScript",
+            "Typescript"
+        ],
+        "libraries": [
+            "React",
+            "React Native",
+            "Redux Toolkit",
+            "Node.js",
+            "Next.js",
+            "Nest.js",
+            "Expo"
+        ],
+        "dbAndBackend": [
+            "REST API",
+            "MongoDB",
+            "MySQL",
+            "PostgreSQL",
+            "Firebase)"
+        ],
+        "tools": [
+            "Git",
+            "GitHub",
+            "Gitlab",
+            "Postman",
+            "Figma"
+        ],
+        "designs": [
+            "Tailwind",
+            "Material UI",
+            "Ant Design",
+            "Tamagui"
+        ],
+        "softSkills": [
+            "Communication",
+            "Clean Code",
+            "Version Control",
+            "Problem Solving",
+            "Debugging"
+        ]
+    },
+    "experience": [
+        {
+            "title": "Frontend Developer (React Native)",
+            "company": "Medisuware Ltd, Dhaka, Bangladesh",
+            "company_url": "https://mediusware.com/team/md-al-mamun",
+            "logo_path": "https://mediusware.com/brand.svg",
+            "duration": "Dec 2023 - Present",
+            "location": "Dhaka, Bangladesh",
+            "description": [
+                "Built a user onboarding system that boosted engagement by 20% and streamlined subscription workflows.",
+                "Developed and deployed cross-platform mobile features using React Native and Expo. Deployed apps to App Store and Google Play.",
+                "Managed Git workflows, enforced PR reviews, and maintained clean, production-ready code across teams."
+            ],
+            "color": "#0879bf"
+        },
+        {
+            "title": "Full Stack Developer (Remote)",
+            "company": "Megahoot Technologies Inc",
+            "company_url": "https://megahoot.com/",
+            "logo_path": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKDTWDTTIx7ayvF78wTFdQeDIh-7hgM07b4QRALA5_nU-nOeCofJkxSrga3_ee1ckxSTY",
+            "duration": "Feb 2022 - Dec 2023",
+            "location": "Remote",
+            "description": [
+                "Built features like user verification badges, link previews, and designation systems to boost engagement and UX.",
+                "Implemented lazy-loading for images and components, improving performance and reducing initial load times.",
+                "Developed and maintained the frontend of a MERN-stack project using React and Redux for scalable state management.",
+                "Collaborated on Node.js + Express APIs, ensuring smooth data flow and tight integration across the app."
+            ],
+            "color": "#0879bf"
+        }
     ],
-    "libraries": [
-      "React",
-      "React Native",
-      "Redux Toolkit",
-      "Node.js",
-      "Next.js",
-      "Nest.js",
-      "Expo"
+    "projects": [
+        {
+            "name": "CompleteGreet",
+            "description": "CompleteGreet is a SASS platform that combines live text chat and video bubbles in perfect harmony, It let's you to create chat bubble and embed it in your website with features like live chats, calls.",
+            "url": "https://completegreet.com",
+            "image": "https://completegreet.com/wp-content/uploads/2024/08/CompleteGreet-logo-bla.webp",
+            "techs": [
+                "Next.js",
+                "Tailwind CSS",
+                "Node.js",
+                "Express.js",
+                "SQL",
+                "Socket.io",
+                "Cloudways",
+                "Cloudinary",
+                "Stripe"
+            ],
+            "works": [
+                "Created SAAS a platform for embeddable video bubbles with live chat features.",
+                "Optimized video loading with CDN, implemented chat bubble API, and added features like scheduling",
+                "Open hours for live chat and call. ",
+                "Tracked analytics (views, clicks, watch time)",
+                "optimized frontend using Next.js (SSR-CSR)",
+                "Migrated production server from DigitalOcean to Render."
+            ]
+        }
     ],
-    "dbAndBackend": [
-      "REST API",
-      "MongoDB",
-      "MySQL",
-      "PostgreSQL",
-      "Firebase)"
+    "leadership": "At Medisuware Ltd, led the development of a React Native Multi-Vendor inventory swapping platform with a focus on:\n- Initiating and structuring the project with React Native CLI, TypeScript, MMKV, andRedux Toolkit.\n- Mentoring junior developers, ensuring clean, scalable code aligned with best practices.\n- Managing Git repositories, Playstore, and TestFlight deployments for testing and production stability.",
+    "extraCurricular": [
+        "Actively shares technical insights on React and JavaScript through LinkedIn, engaging with the developer community.",
+        "Avid guitar player, exploring diverse music genres and styles."
     ],
-    "tools": [
-      "Git",
-      "GitHub",
-      "Gitlab",
-      "Postman",
-      "Figma"
-    ],
-    "designs": [
-      "Tailwind",
-      "Material UI",
-      "Ant Design",
-      "Tamagui"
-    ],
-    "softSkills": [
-      "Communication",
-      "Clean Code",
-      "Version Control",
-      "Problem Solving",
-      "Debugging"
+    "education": {
+        "school": "University of Global Village, Barisal, Bangladesh.",
+        "degree": "B.Sc. in Computer Science and Engineering (Dropout))",
+        "duration": "2019 - 2020",
+        "cgpa": "3.00/4.00",
+        "creditedScore": "70",
+        "courseWorks": [
+            "DBMS",
+            "DSA",
+            "OOP",
+            "C Programming",
+            "Algorithm Design"
+        ]
+    },
+    "works": [
+        {
+            "title": "Complete Greet",
+            "description": "A website that lets you create a chat bubble and embeds it in your website with features like live chats, and calls.",
+            "url": "https://completegreet.com",
+            "images": [
+                "https://i.ibb.co/Wk10Fsj/completegreet1.png",
+                "https://i.ibb.co/VTv3MgZ/completegreet2.png",
+                "https://i.ibb.co/ZMn2qgz/completegreet3.png"
+            ],
+            "techs": [
+                "React",
+                "Next.js",
+                "Redux",
+                "Tailwind CSS",
+                "Node.js",
+                "Express.js",
+                "SQLite",
+                "Stripe API"
+            ]
+        },
+        {
+            "title": "Hoodex",
+            "description": "A Decentralized Digital Asset Swapping System",
+            "url": "https://www.hootdex.net/",
+            "images": [
+                "https://raw.githubusercontent.com/mamun-mahmood/hootdexFrontend/master/Screenshot%20from%202023-12-03%2023-58-34.png"
+            ],
+            "techs": [
+                "React",
+                "ApexCharts",
+                "MUI",
+                "Nodejs",
+                "Solidity",
+                "Crypto Wallets"
+            ]
+        },
+        {
+            "title": "News (PWA)",
+            "description": "Offile Capable web application with Firebase Authentication and React feature",
+            "url": "https://newspaw.vercel.app",
+            "images": [
+                "https://fast.image.delivery/ekqpruv.png?source=image.delivery"
+            ],
+            "techs": [
+                "Vite React",
+                "Fireabse",
+                "Material UI",
+                "Web Workers",
+                "Offline Sync"
+            ]
+        },
+        {
+            "title": "MyFlix",
+            "description": "Find the latest movies and TV shows in no time",
+            "url": "https://myflix-lake-six.vercel.app/",
+            "images": [
+                "https://i.ibb.co/VBcjXFG/myflix1.png",
+                "https://i.ibb.co/JyhxyQq/myflix2.png",
+                "https://i.ibb.co/gj7xbLM/myflix3.png"
+            ],
+            "techs": [
+                "React",
+                "Redux",
+                "Styled Components",
+                "The Movie DB API"
+            ]
+        },
+        {
+            "title": "WhiteBoard",
+            "description": "Whiteboard app with basic features. Creating shapes, drawing, and adding text.",
+            "url": "https://whiteboardx.vercel.app/",
+            "images": [
+                "https://raw.githubusercontent.com/mamun-mahmood/whiteboard/master/docs/ss/p1.png"
+            ],
+            "techs": [
+                "React",
+                "TailWind CSS",
+                "React Konva",
+                "Nodejs",
+                "Express",
+                "MongoDB"
+            ]
+        },
+        {
+            "title": "BlueTrade",
+            "description": "A Dashboard Layout built with React, ApexCharts, and Vanilla CSS.",
+            "url": "https://bluetrade.vercel.app/",
+            "images": [
+                "https://raw.githubusercontent.com/mamun-mahmood/react-dashboard/main/docs/images/Screenshot%20from%202023-08-31%2020-52-00.png"
+            ],
+            "techs": [
+                "React",
+                "ApexCharts",
+                "Vanilla CSS"
+            ]
+        }
     ]
-  },
-  "experience": [
-    {
-      "title": "Frontend Developer (React Native)",
-      "company": "Medisuware Ltd, Dhaka, Bangladesh",
-      "company_url": "https://mediusware.com/team/md-al-mamun",
-      "logo_path": "https://mediusware.com/brand.svg",
-      "duration": "Dec 2023 - Present",
-      "location": "Dhaka, Bangladesh",
-      "description": [
-        "Built a user onboarding system that boosted engagement by 20% and streamlined subscription workflows.",
-        "Developed and deployed cross-platform mobile features using React Native and Expo. Deployed apps to App Store and Google Play.",
-        "Managed Git workflows, enforced PR reviews, and maintained clean, production-ready code across teams."
-      ],
-      "color": "#0879bf"
-    },
-    {
-      "title": "Full Stack Developer (Remote)",
-      "company": "Megahoot Technologies Inc",
-      "company_url": "https://megahoot.com/",
-      "logo_path": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKDTWDTTIx7ayvF78wTFdQeDIh-7hgM07b4QRALA5_nU-nOeCofJkxSrga3_ee1ckxSTY",
-      "duration": "Feb 2022 - Dec 2023",
-      "location": "Remote",
-      "description": [
-        "Built features like user verification badges, link previews, and designation systems to boost engagement and UX.",
-        "Implemented lazy-loading for images and components, improving performance and reducing initial load times.",
-        "Developed and maintained the frontend of a MERN-stack project using React and Redux for scalable state management.",
-        "Collaborated on Node.js + Express APIs, ensuring smooth data flow and tight integration across the app."
-      ],
-      "color": "#0879bf"
-    }
-  ],
-  "projects": [
-    {
-      "name": "CompleteGreet",
-      "description": "CompleteGreet is a SASS platform that combines live text chat and video bubbles in perfect harmony, It let's you to create chat bubble and embed it in your website with features like live chats, calls.",
-      "url": "https://completegreet.com",
-      "image": "https://completegreet.com/wp-content/uploads/2024/08/CompleteGreet-logo-bla.webp",
-      "techs": [
-        "Next.js",
-        "Tailwind CSS",
-        "Node.js",
-        "Express.js",
-        "SQL",
-        "Socket.io",
-        "Cloudways",
-        "Cloudinary",
-        "Stripe"
-      ],
-      "works": [
-        "Created SAAS a platform for embeddable video bubbles with live chat features.",
-        "Optimized video loading with CDN, implemented chat bubble API, and added features like scheduling",
-        "Open hours for live chat and call. ",
-        "Tracked analytics (views, clicks, watch time)",
-        "optimized frontend using Next.js (SSR-CSR)",
-        "Migrated production server from DigitalOcean to Render."
-      ]
-    }
-  ],
-  "leadership": "At Medisuware Ltd, led the development of a React Native Multi-Vendor inventory swapping platform with a focus on:\n- Initiating and structuring the project with React Native CLI, TypeScript, MMKV, andRedux Toolkit.\n- Mentoring junior developers, ensuring clean, scalable code aligned with best practices.\n- Managing Git repositories, Playstore, and TestFlight deployments for testing and production stability.",
-  "extraCurricular": [
-    "Actively shares technical insights on React and JavaScript through LinkedIn, engaging with the developer community.",
-    "Avid guitar player, exploring diverse music genres and styles."
-  ],
-  "education": {
-    "school": "University of Global Village, Barisal, Bangladesh.",
-    "degree": "B.Sc. in Computer Science and Engineering (Dropout))",
-    "duration": "2019 - 2020",
-    "cgpa": "3.00/4.00",
-    "creditedScore": "70",
-    "courseWorks": [
-      "DBMS",
-      "DSA",
-      "OOP",
-      "C Programming",
-      "Algorithm Design"
-    ]
-  },
-  "works": [
-    {
-      "title": "Complete Greet",
-      "description": "A website that lets you create a chat bubble and embeds it in your website with features like live chats, and calls.",
-      "url": "https://completegreet.com",
-      "images": [
-        "https://i.ibb.co/Wk10Fsj/completegreet1.png",
-        "https://i.ibb.co/VTv3MgZ/completegreet2.png",
-        "https://i.ibb.co/ZMn2qgz/completegreet3.png"
-      ],
-      "techs": [
-        "React",
-        "Next.js",
-        "Redux",
-        "Tailwind CSS",
-        "Node.js",
-        "Express.js",
-        "SQLite",
-        "Stripe API"
-      ]
-    },
-    {
-      "title": "Hoodex",
-      "description": "A Decentralized Digital Asset Swapping System",
-      "url": "https://www.hootdex.net/",
-      "images": [
-        "https://raw.githubusercontent.com/mamun-mahmood/hootdexFrontend/master/Screenshot%20from%202023-12-03%2023-58-34.png"
-      ],
-      "techs": [
-        "React",
-        "ApexCharts",
-        "MUI",
-        "Nodejs",
-        "Solidity",
-        "Crypto Wallets"
-      ]
-    },
-    {
-      "title": "New app (PWA)",
-      "description": "Offile Capable web applicaton with Authentication and reaction feature",
-      "url": "https://completegreet.com",
-      "images": [
-        "https://fast.image.delivery/ekqpruv.png?source=image.delivery"
-      ],
-      "techs": [
-        "ViteReact",
-        "Fireabse",
-        "Material UI"
-      ]
-    },
-    {
-      "title": "MyFlix",
-      "description": "Find the latest movies and TV shows in no time",
-      "url": "https://myflix-lake-six.vercel.app/",
-      "images": [
-        "https://i.ibb.co/VBcjXFG/myflix1.png",
-        "https://i.ibb.co/JyhxyQq/myflix2.png",
-        "https://i.ibb.co/gj7xbLM/myflix3.png"
-      ],
-      "techs": [
-        "React",
-        "Redux",
-        "Styled Components",
-        "The Movie DB API"
-      ]
-    },
-    {
-      "title": "ChatterFly",
-      "description": "A RealTime Chat Application. Built with React, MUI, and Firebase.",
-      "url": "https://chatterfly.vercel.app/",
-      "images": [
-        "https://raw.githubusercontent.com/mamun-mahmood/chatterfly/main/docs/Screenshot%20from%202023-08-08%2009-45-25.png",
-        "https://raw.githubusercontent.com/mamun-mahmood/chatterfly/main/docs/Screenshot%20from%202023-08-08%2009-45-01.png"
-      ],
-      "techs": [
-        "React",
-        "Redux",
-        "MUI",
-        "Firebase"
-      ]
-    },
-    {
-      "title": "WhiteBoard",
-      "description": "Whiteboard app with basic features. Creating shapes, drawing, and adding text.",
-      "url": "https://whiteboardx.vercel.app/",
-      "images": [
-        "https://raw.githubusercontent.com/mamun-mahmood/whiteboard/master/docs/ss/p1.png"
-      ],
-      "techs": [
-        "React",
-        "TailWind CSS",
-        "React Konva",
-        "Nodejs",
-        "Express",
-        "MongoDB"
-      ]
-    },
-    {
-      "title": "BlueTrade",
-      "description": "A Dashboard Layout built with React, ApexCharts, and Vanilla CSS.",
-      "url": "https://bluetrade.vercel.app/",
-      "images": [
-        "https://raw.githubusercontent.com/mamun-mahmood/react-dashboard/main/docs/images/Screenshot%20from%202023-08-31%2020-52-00.png"
-      ],
-      "techs": [
-        "React",
-        "ApexCharts",
-        "Vanilla CSS"
-      ]
-    },
-    {
-      "title": "Whiteboard",
-      "description": "A whiteboard app, where you can draw and save things online.",
-      "url": "https://whiteboardx.vercel.app/",
-      "images": [
-        "https://github.com/mamun-mahmood/whiteboard/raw/master/docs/ss/p2.png",
-        "https://github.com/mamun-mahmood/whiteboard/raw/master/docs/ss/p1.png"
-      ],
-      "techs": [
-        "MERN stack"
-      ]
-    }
-  ]
 }
